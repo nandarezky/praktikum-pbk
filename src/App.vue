6b76788e189f319ae39819c097a8a9c8e7f3a37b
unknown <nandarezky01002@gmail.com>
2024-04-28 19:03:53 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <task v-bind:tasks="tasks"></task>
+  </div>
+</template>
+
+<script>
+import Task from "./components/Task";
+
+export default {
+  name: "App",
+  components: {
+    Task,
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          title: "Membuat tugas pbk",
+          completed: true,
+        },
+        {
+          id: 2,
+          title: "Membaca buku",
+          completed: true,
+        },
+        {
+          id: 3,
+          title: "Membuat tugas pbo",
+          completed: false,
+        },
+        {
+          id: 4,
+          title: "Makan",
+          completed: false,
+        },
+        
+      ],
+    };
+  },
+};
+</script>
