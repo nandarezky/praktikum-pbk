f6da7e9a4b2472fa3ae6623d4e132a8ae3c090ea
unknown <nandarezky01002@gmail.com>
2024-05-07 21:36:56 +0700

    first commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
+import './main.css'
 
-Vue.config.productionTip = false
-
-import './assets/css/main.css';
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
